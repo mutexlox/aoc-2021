858db43e723e486f67d9366742304f6bebaf2b5c
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2021-11-26 09:50:31 -0500

    simplify quine

--- src/quine.rs
+++ src/quine.rs
@@ -1,15 +1,13 @@
 static S: &str = r#"static S: &str = r#"S_HERE"POUND_HERE;
 fn main() {
-    let p = "\x23";
     println!(
         "{}",
-        S.replacen("POUND_HERE", p, 1).replacen("S_HERE", S, 1)
+        S.replacen("POUND_HERE", "\x23", 1).replacen("S_HERE", S, 1)
     );
 }"#;
 fn main() {
-    let p = "\x23";
     println!(
         "{}",
-        S.replacen("POUND_HERE", p, 1).replacen("S_HERE", S, 1)
+        S.replacen("POUND_HERE", "\x23", 1).replacen("S_HERE", S, 1)
     );
 }
