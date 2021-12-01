5439ee6f3b1b6aa5dee223994eb5077e72f3fb3d
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2021-12-01 08:31:49 -0500

    do day01 part 2

--- src/day01_sonar.rs
+++ src/day01_sonar.rs
@@ -11,6 +11,22 @@ fn count_increases(input: &[i64]) -> i64 {
     increases
 }
 
+fn count_increases_window(input: &[i64], win_size: usize) -> i64 {
+    let mut increases = 0;
+    let mut window = 0;
+    for i in 0..win_size {
+        window += input[i];
+    }
+    for i in win_size..input.len() {
+        let new_win = window - input[i - win_size] + input[i];
+        if new_win > window {
+            increases += 1;
+        }
+        window = new_win;
+    }
+    increases
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2);
@@ -22,4 +38,5 @@ fn main() {
         .map(|s| s.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
     println!("{}", count_increases(&input));
+    println!("{}", count_increases_window(&input, 3));
 }
