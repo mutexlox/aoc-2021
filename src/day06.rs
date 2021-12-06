54695013e03654bd0dc709bf501e84febdcf279c
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2021-12-05 21:14:20 -0800

    do day 06 pt 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,24 +1,22 @@
 use std::env;
 use std::fs;
 
-fn num_fish_after(input: &[i64], days: i64) -> usize {
-    let mut fishies = input.to_vec();
+fn num_fish_after(input: &[usize], days: i64) -> usize {
+    // track number of fish of each age
+    let mut fishies = [0; 9];
+    for x in input.iter() {
+        fishies[*x] += 1;
+    }
     for _ in 0..days {
-        let mut to_add = 0;
-        for fish in fishies.iter_mut() {
-            if *fish == 0 {
-                *fish = 6;
-                to_add += 1;
-            } else {
-                *fish -= 1;
-            }
+        let zero_count = fishies[0];
+        for i in 1..fishies.len() {
+            fishies[i - 1] = fishies[i];
         }
-        for _ in 0..to_add {
-            fishies.push(8);
-        }
+        fishies[6] += zero_count;
+        fishies[8] = zero_count;
     }
 
-    fishies.len()
+    fishies.iter().sum()
 }
 
 fn main() {
@@ -29,7 +27,7 @@ fn main() {
     let input = input_str
         .trim()
         .split(',')
-        .map(|s| s.parse::<i64>().unwrap())
+        .map(|s| s.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
     println!("{}", num_fish_after(&input, 80));
     println!("{}", num_fish_after(&input, 256));
